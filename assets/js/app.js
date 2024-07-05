04e69702c7f2d93bb7174c258ca997c34c213328
MCavieres <maca.c.91@gmail.com>
2024-07-04 20:20:54 -0400

    se hace logica de desencriptar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,14 @@
 const btnEncriptar = document.querySelector("#btnEncriptar");
+const btnDesencriptar = document.querySelector("#btnDesencriptar");
+const msgEncript = document.querySelector("#msgEncript");
+const btnCopiar = document.querySelector(".main__der__copiar");
+const derImg = document.querySelector(".main__der__img");
+const derH5 = document.querySelector(".main__der__h5");
+const derP = document.querySelector(".main__der__p");
+const alert = document.querySelector(".main__izq__alert");
+const textArea = document.querySelector("#msgUsuario");
 
-const arrayEncript = {
+const claves = {
     a: "ai",
     e: "enter",
     i: "imes",
@@ -8,48 +16,61 @@ const arrayEncript = {
     u: "ufat",
 };
 
-// btnEncriptar.addEventListener("click", () => {
-//     const msgUsuario = document.querySelector("#msgUsuario").value;
-//     let resultado = "";
-
-//     for (let i = 0; i < msgUsuario.length; i++) {
-//         if (msgUsuario[i].includes("a")) {
-//             const encript = arrayEncript.a;
-//             resultado += encript;
-//         } else {
-//             resultado += msgUsuario[i];
-//         }
-//     }
-//     console.log(resultado);
-// });
-
-// btnEncriptar.addEventListener("click", () => {
-//     const msgUsuario = document.querySelector("#msgUsuario").value;
-
-//     const arrayMsg = msgUsuario.split("");
-//     const msgEncript = arrayMsg.map((item) => {
-//         if (item === "a") {
-//             item = "ai";
-//         } else {
-//             item;
-//         }
-//         return item;
-//     });
-
-//     const stringMsg = msgEncript.join("");
-//     console.log(stringMsg);
-// });
+const valores = [
+    {
+        ai: "a",
+        enter: "e",
+        imes: "i",
+        ober: "o",
+        ufat: "u",
+    },
+];
 
 btnEncriptar.addEventListener("click", () => {
-    const msgUsuario = document.querySelector("#msgUsuario").value;
+    const msgUsuario = textArea.value.trim();
     let encriptado = "";
+    idem(msgUsuario);
 
     for (let j = 0; j < msgUsuario.length; j++) {
-        if (arrayEncript.hasOwnProperty(msgUsuario[j])) {
-            encriptado += arrayEncript[msgUsuario[j]];
+        if (claves.hasOwnProperty(msgUsuario[j])) {
+            encriptado += claves[msgUsuario[j]];
         } else {
             encriptado += msgUsuario[j];
         }
     }
-    console.log(encriptado);
+    msgEncript.textContent = encriptado;
+    textArea.value = "";
 });
+
+btnCopiar.addEventListener("click", () => {
+    console.log("copy");
+});
+
+btnDesencriptar.addEventListener("click", () => {
+    let msgUsuario = textArea.value.trim();
+
+    idem(msgUsuario);
+
+    Object.entries(claves).forEach(([key, value]) => {
+        msgUsuario = msgUsuario.split(value).join(key);
+    });
+
+    msgEncript.textContent = msgUsuario;
+    textArea.value = "";
+});
+
+function idem(msgUsuario) {
+    const regex = /^[a-z\s]+$/;
+    if (!msgUsuario || !regex.test(msgUsuario)) {
+        alert.style.display = "block";
+        return;
+    } else {
+        alert.style.display = "none";
+    }
+
+    msgEncript.style.display = "block";
+    btnCopiar.style.display = "block";
+    derImg.style.display = "none";
+    derH5.style.display = "none";
+    derP.style.display = "none";
+}
